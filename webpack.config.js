e4b0f31da53cedad063cd43ce8f085507c04f515
Mariano Benedettini <mariano@glide.com>
2021-01-25 15:48:59 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/',
+    filename: '[name].[contenthash].js',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[id].[contenthash].css',
+        // ignoreOrder: true
+      }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(less|css)$/,
+        use: [
+          // {
+          //   loader: MiniCssExtractPlugin.loader
+          // },
+          {
+            loader: 'style-loader',
+            options: {
+              esModule: true,
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+              lessOptions: {
+                // modifyVars: theme,
+                javascriptEnabled: true,
+              }
+            },
+          },
+        ],
+      },
+    ]
+  }
+};
